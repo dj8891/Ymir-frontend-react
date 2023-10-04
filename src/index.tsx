3eafc406edd489bb19aee0b99399482d1ca2e0b5
Michael Sullivan <michaelrsullivan1@gmail.com>
2023-10-04 11:28:27 -0500

    More generic cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from 'react-dom/client';
 
-// The issue with using a common id like “root” for injecting widgets, as it may clash with other elements with the same id,
-//  and emphasizes using class identifiers and data attributes to dynamically inject multiple instances of a widget into divs,
-//  allowing for more flexibility and control over parameters.
-const widgetDivs: NodeListOf<HTMLDivElement> =
-  document.querySelectorAll(".ymir-widget");
-
-widgetDivs.forEach((div: HTMLDivElement) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App symbol={div.dataset.symbol ?? ""} />
-    </React.StrictMode>,
-    div
-  );
-});
+const container = document.getElementById('ymir-application');
+const root = createRoot(container!);
+root.render(<App />);
 
 reportWebVitals();
