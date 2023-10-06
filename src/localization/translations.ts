8e1cec68f48e12ac64f20f5834bc4dff8f5cf55a
Michael Sullivan <michaelrsullivan1@gmail.com>
2023-10-06 15:54:37 -0500

    Create specific files for each category of translation

--- src/localization/translations.ts
+++ src/localization/translations.ts
@@ -1,54 +1,24 @@
-
 // In the future this can potentially be managed in a UI:
-//   https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+//   https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui
+
+import navigation from "./translation-categories/navigation";
+import basePage from "./translation-categories/base-page";
+import errorPage from "./translation-categories/error-page";
 
 const translations = {
     resources: {
         en: {
             translation: {
-                navigation: {
-                    chats: "Chats",
-                    reports: "Reports",
-                    members: "Members",
-                    manageModerator: "Manage Moderator",
-                    database: "Database",
-                    upload: "Upload",
-                    helpCenter: "Help Center",
-                    settings: "Settings",
-                    moderators: "Moderators",
-                    dashboard: "Dashboard"
-                },
-                basePage: {
-                    available: 'Available',
-                },
-                errorPage: {
-                    oops: "Oops!",
-                    unexpectedError: "An unexpected error has occurred.",
-                }
+                navigation: navigation.en,
+                basePage: basePage.en,
+                errorPage: errorPage.en
             }
         },
         de: {
             translation: {
-                navigation: {
-                    // This is just to show that we can have different translations for different languages
-                    chats: "Chats (de)",
-                    reports: "Reports (de)",
-                    members: "Members (de)",
-                    manageModerator: "Manage Moderator (de)",
-                    database: "Database (de)",
-                    upload: "Upload (de)",
-                    helpCenter: "Help Center (de)",
-                    settings: "Settings (de)",
-                    moderators: "Moderators (de)",
-                    dashboard: "Dashboard (de)"
-                },
-                basePage: {
-                    available: 'Available (de)',
-                },
-                errorPage: {
-                    oops: "Oops! (de)",
-                    unexpectedError: "An unexpected error has occurred. (de)",
-                }
+                navigation: navigation.de,
+                basePage: basePage.de,
+                errorPage: errorPage.de
             }
         }
     },
