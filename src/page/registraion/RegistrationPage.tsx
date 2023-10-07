174fe3b118d0240132f879c528d28d19bcb9c4f4
Michael Sullivan <michaelrsullivan1@gmail.com>
2023-10-07 08:09:36 -0500

    Set up basic registration page structure

--- src/page/registraion/RegistrationPage.tsx
+++ src/page/registraion/RegistrationPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 import Colors from "../../constants/colors";
+import { ReactComponent as TemporaryLogo } from "./assets/temporary-logo.svg";
 
 interface RegistrationPageProps {
   className?: string;
@@ -12,14 +13,29 @@ const RegistrationPage = styled(({ className }: RegistrationPageProps) => {
 
   return (
     <div id="registration-page" className={className}>
-      <p>Registration page will go here</p>
+      <div className="registration-area">
+        <TemporaryLogo className="registration-logo" />
+      </div>
     </div>
   );
 })`
   height: 100%;
+  padding: 1rem;
 
-  p {
-    text-align: center;
+  .registration-area {
+    background: ${Colors.backgroundWhite};
+    height: 83%;
+    margin: 0 auto;
+    border-radius: 0.5rem;
+    max-width: 60rem;
+    padding: 2rem;
+
+    .registration-logo {
+      margin: 0 auto;
+      width: 3rem;
+      height: 3rem;
+      display: block;
+    }
   }
 `;
 
